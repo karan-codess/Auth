1b867b6972d9487f71b910ca8dce80f862e3a4df
karan-codess <karansinghyadav7172@gmail.com>
2026-03-16 16:21:10 +0530

    update code

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoute.js";
+import cors from  "cors"
 import cookieParser from "cookie-parser";
 dotenv.config();
 
 let app = express();
 let port = process.env.PORT || 4000;
 app.use(express.json());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 app.use(cookieParser())
 app.use("/api", authRouter);
 
